ef878d64ead946a197e2180717a722ce663ca358
lessw2020 <lessw@etrillium.com>
2022-08-13 23:26:57 +0000

    add models

---
+++ multi_gpu/bloom.py
@@ -0,0 +1,20 @@
+import torch
+model_name = "bigscience/bloom-3b"
+
+text = "Hello my name is"
+max_new_tokens = 50
+
+def generate_text(model, tokenizer, input_text):
+    encoded_input = tokenizer(input_text, return_tensors="pt")
+    output_seq = model.generate(input_ids = encoded_input(=['input_ids',].cuda()))
+    return tokenizer.decode(output_seq[0],skip_special_tokens=True)
+
+from transformers import pipeline
+pipe = pipeline(model=model_name, model_kwargs = {"device_map": "auto", "load_in_8bit": True}, max_new_tokens=max_new_tokens)
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+model_8bit = AutoModelForCausalLM.from_pretrained(name, device_map="auto", load_in_8bit=True)
+tokenizer = AutoTokenizer.from_pretrained(name)
+generate_from_model(model_8bit, tokenizer)
